252ecaeec4f43967e080bdca6f7625ca25ecf159
Keith <keithpetrone@atlanticbb.net>
2016-02-16 13:47:48 -0700

    Remove unnecessary usings

--- OneBox/OneBox/Models/GoogleDriveDownloader.cs
+++ OneBox/OneBox/Models/GoogleDriveDownloader.cs
@@ -3,7 +3,6 @@ using Google.Apis.Drive.v2;
 using Google.Apis.Drive.v2.Data;
 using Google.Apis.Services;
 using OneBox.Models;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
